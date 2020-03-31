c05e9afa4e8b6d11cd287e07a92644174a5ccfd7
mit1800 <48518460+mit1800@users.noreply.github.com>
2020-03-31 14:45:49 +0530

    updated

--- app/src/main/java/com/example/equipments/product/ProductListActivity.java
+++ app/src/main/java/com/example/equipments/product/ProductListActivity.java
@@ -1,14 +1,24 @@
 package com.example.equipments.product;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.Configuration;
+import android.graphics.Rect;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -24,7 +34,10 @@ public class ProductListActivity extends BaseActivity {
     ImageView ivSearchProduct, ivBackButton;
     ProductListRecyclerViewAdapter productAdapter;
     String[] demoProductName = new String[sampleTestingLimit];
+    int[] imagePosition = new int[sampleTestingLimit];
     RecyclerView rvProducts;
+    LinearLayout buttonTestingLayout,imageTestingLayout;
+    Button btnTesting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +48,9 @@ public class ProductListActivity extends BaseActivity {
         ivSearchProduct = (ImageView) findViewById(R.id.ivSearchProduct);
         ivBackButton = (ImageView) findViewById(R.id.ivBackActivity);
         rvProducts = findViewById(R.id.rvProductList);
+        buttonTestingLayout = (LinearLayout) findViewById(R.id.btnTestingLayout);
+        imageTestingLayout = (LinearLayout) findViewById(R.id.imagePricePopupLayout);
+        btnTesting = (Button) findViewById(R.id.btnTesting);
 
         ivBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,14 +60,32 @@ public class ProductListActivity extends BaseActivity {
             }
         });
 
-        ActionBar actionBar = getSupportActionBar();  //To create custom ActionBar with drawable
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setDisplayShowHomeEnabled(false);
-            actionBar.setDisplayShowTitleEnabled(true);
-            actionBar.setDisplayUseLogoEnabled(false);
-            actionBar.setHomeAsUpIndicator(R.drawable.ic_back_arrow);
-        }
+        btnTesting.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.d("hover", "Bring yor cursor over the button");
+                if(event.getAction()==MotionEvent.ACTION_UP)
+                {
+                    //instantiate the popup.xml layout file
+                    LayoutInflater layoutInflater = (LayoutInflater) ProductListActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    View customView = layoutInflater.inflate(R.layout.productlist_popup_menu,null);
+
+                    //instantiate popup window
+                    PopupWindow popupWindow = new PopupWindow(customView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+                    popupWindow.setFocusable(true);
+                    popupWindow.setBackgroundDrawable(new ColorDrawable());
+                    popupWindow.setOutsideTouchable(true);
+
+                    //display the popup window
+                    popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+
+                    return true;
+                }
+
+                return false;
+            }
+        });
 
         prepareDemoData();
     }
@@ -59,7 +93,8 @@ public class ProductListActivity extends BaseActivity {
     private void prepareDemoData() {
         for (int i = 0; i < demoProductName.length; i++) {
             demoProductName[i] = String.valueOf(i);
-        }
+            imagePosition[i] = i+1;
+         }
         if (isTabletDevice()) {
             rvProducts.setLayoutManager(new GridLayoutManager(this, 3));
         } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
@@ -67,7 +102,7 @@ public class ProductListActivity extends BaseActivity {
         } else {
             rvProducts.setLayoutManager(new GridLayoutManager(this, 2));
         }
-        productAdapter = new ProductListRecyclerViewAdapter(this, demoProductName);
+        productAdapter = new ProductListRecyclerViewAdapter(this, demoProductName,imagePosition);
         rvProducts.setAdapter(productAdapter);
     }
 
@@ -75,10 +110,12 @@ public class ProductListActivity extends BaseActivity {
 
         String[] productName;
         LayoutInflater mInflater;
+        int[] imageId;
 
-        ProductListRecyclerViewAdapter(Context context, String[] data) {
+        ProductListRecyclerViewAdapter(Context context, String[] data, int[] imageId) {
             this.mInflater = LayoutInflater.from(context);
             this.productName = data;
+            this.imageId = imageId;
         }
 
         @Override
@@ -89,10 +126,19 @@ public class ProductListActivity extends BaseActivity {
             return new ViewHolder(view);
         }
 
-        // binds the data to the TextView in each cell
         @Override
         public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
             holder.tvProductName.setText(productName[position]);
+
+            holder.ivPricePopupMenu.setTag(new Integer(position));   //To get particular position of item in grid
+
+            holder.ivPricePopupMenu.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    initiatePopupWindow(v);
+                }
+            });
+
             if (position % 2 == 0) {
                 holder.ivProductImage.setImageResource(R.drawable.infra_bazaar_backhoe);
             } else if (position % 3 == 0) {
@@ -108,6 +154,60 @@ public class ProductListActivity extends BaseActivity {
             });
         }
 
+        private void initiatePopupWindow(View view) {
+
+
+            Integer position = (Integer)view.getTag();  //Get position of particular item
+
+            //instantiate the popup.xml layout file
+            LayoutInflater layoutInflater = (LayoutInflater) ProductListActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View customView = layoutInflater.inflate(R.layout.productlist_popup_menu,null);
+
+            //instantiate popup window
+            final PopupWindow popupWindow = new PopupWindow(customView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+            popupWindow.setFocusable(true);
+            popupWindow.setTouchable(true);
+            popupWindow.setBackgroundDrawable(new ColorDrawable());
+            popupWindow.setOutsideTouchable(true);
+
+            //display the popup window
+            popupWindow.showAtLocation(buttonTestingLayout,  Gravity.NO_GRAVITY,locateView(view).right,locateView(view).bottom);
+          //  popupWindow.showAtLocation(buttonTestingLayout,  Gravity.NO_GRAVITY,locateView(view).top,locateView(view).left);
+        }
+
+        public Rect locateView(View v)
+        {
+            int[] loc_int = imageId;
+            if (v == null) return null;
+            try
+            {
+                v.getLocationOnScreen(loc_int);
+            } catch (NullPointerException npe)
+            {
+                //Happens when the view doesn't exist on screen anymore.
+                return null;
+            }
+            Rect location = new Rect();
+            /*location.left = loc_int[0];
+            location.top = loc_int[1];
+            location.right = location.left + v.getWidth();
+            location.bottom = location.top + v.getHeight();*/
+
+            //For right and bottom side popup
+            location.left = loc_int[0];
+            location.top = loc_int[1];
+            location.right = location.left + v.getWidth();
+            location.bottom = location.top + v.getHeight();
+
+            /*location.left = location.right + v.getWidth();
+            location.top = location.top + v.getWidth();
+            location.right = loc_int[0];
+            location.bottom = loc_int[1];
+*/
+            return location;
+        }
+
         @Override
         public int getItemCount() {
             return productName.length;
@@ -115,12 +215,14 @@ public class ProductListActivity extends BaseActivity {
 
         public class ViewHolder extends RecyclerView.ViewHolder {
             TextView tvProductName;
-            ImageView ivProductImage;
+            ImageView ivProductImage, ivPricePopupMenu;
 
             ViewHolder(View itemView) {
                 super(itemView);
                 tvProductName = itemView.findViewById(R.id.tvProductName);
                 ivProductImage = itemView.findViewById(R.id.ivProductImage);
+                ivPricePopupMenu = itemView.findViewById(R.id.ivPricePopupMenu);
+
             }
 
         }
